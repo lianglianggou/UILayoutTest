8b289fe27d7c788a228c9cc3af029d005698d0e1
lianglianggou <317767301qq.com>
2018-10-09 23:05:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/uilayouttest/UILayoutTest.java
@@ -0,0 +1,13 @@
+package com.example.uilayouttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UILayoutTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uilayout_test);
+    }
+}
