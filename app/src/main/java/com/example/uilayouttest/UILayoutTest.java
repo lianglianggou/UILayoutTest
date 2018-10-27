1508ecec9e0e15360d02b58726ae36726e1fee9d
lianglianggou <317767301qq.com>
2018-10-27 09:49:35 +0800

    新增点击

--- app/src/main/java/com/example/uilayouttest/UILayoutTest.java
+++ app/src/main/java/com/example/uilayouttest/UILayoutTest.java
@@ -2,12 +2,194 @@ package com.example.uilayouttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UILayoutTest extends AppCompatActivity {
-
+    TextView show;
+    Button bn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uilayout_test);
+        show=(TextView)findViewById(R.id.text1);
+        bn=(Button)findViewById(R.id.button1);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击1");
+            }
+        });
+        bn=(Button)findViewById(R.id.button2);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击2");
+            }
+        });
+        bn=(Button)findViewById(R.id.button3);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击3");
+            }
+        });bn=(Button)findViewById(R.id.button4);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击4");
+            }
+        });bn=(Button)findViewById(R.id.button5);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击1");
+            }
+        });bn=(Button)findViewById(R.id.button6);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击1");
+            }
+        });bn=(Button)findViewById(R.id.button7);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击1");
+            }
+        });bn=(Button)findViewById(R.id.button8);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击1");
+            }
+        });bn=(Button)findViewById(R.id.button9);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击1");
+            }
+        });bn=(Button)findViewById(R.id.button10);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("点击1");
+            }
+        });bn=(Button)findViewById(R.id.button11);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button12);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button13);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button14);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button15);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button16);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button17);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button18);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button19);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button20);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button21);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button22);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button23);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button24);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });bn=(Button)findViewById(R.id.button25);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show.setText("ce");
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 }
